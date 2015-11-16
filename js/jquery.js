c155f9640a9fbf5b5443418984285c50e72f3c83
Kafia Ahmed <kafia@sutlej.net>
2015-11-16 23:17:45 +0500

    Click Event Effect on what we do page

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,24 @@
-// jQuery //
+// jQuery Hover Effect//
 
 $(document).ready(function(){
     $("h1.what-we-do").hover(function(){
         $(this).css("background-color", "#f16529");
 		$(this).css("text-align", "center");
+		$(this).css("color", "white");
         }, function(){
         $(this).css("background-color", "white");
 		$(this).css("text-align", "left");
+		$(this).css("color", "black");
+    });
+});
+// jQuery Click Effect//
+
+$(document).ready(function(){
+    $("button").click(function(){
+         var div = $("div.animate"); 
+		div.css("background-color","#f16529");
+		div.css("color","white");
+        div.animate({left: '250px',height:'100px',width:'500px'}, "slow");
+        div.animate({fontSize: '3em'}, "slow");
     });
 });
