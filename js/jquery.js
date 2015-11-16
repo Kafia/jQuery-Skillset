e37f25306a778c755a5042bc92e6bde442c4fb57
Kafia Ahmed <kafia@sutlej.net>
2015-11-17 00:15:45 +0500

    Two more effects of choice

--- js/jquery.js
+++ js/jquery.js
@@ -22,3 +22,31 @@ $(document).ready(function(){
         div.animate({fontSize: '3em'}, "slow");
     });
 });
+// jQuery Effect on If/Else//
+
+$(document).ready(function(){ 
+
+	var n = $("div.condition").length;
+
+	if (n < 2) {
+		$("div.condition").css("background", "green");
+	} 
+	else {
+		$("div.condition").css("background", "orange");
+	}
+
+});
+// jQuery Effect Fade To//
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").fadeTo("slow", 0.15);
+        $("#div2").fadeTo("slow", 0.4);
+        $("#div3").fadeTo("slow", 0.5);
+    });
+});
+// jQuery Effect Slide Toggle//
+$(document).ready(function(){
+    $(".flip").click(function(){
+        $(".panel").slideToggle("slow");
+    });
+});
