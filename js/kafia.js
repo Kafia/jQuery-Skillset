20ff86413b5535d9e510b88828182fa1c7fa247d
Kafia Ahmed <kafia@sutlej.net>
2015-11-18 11:27:27 +0500

    Click Event

--- js/kafia.js
+++ js/kafia.js
@@ -1,7 +1,14 @@
 // Kafia jQuery //
 $(document).ready(function(){
+	
     $("#Button1").click(function(){
         $("p.hide").hide(1000);
         alert("The paragraph is now hidden");
+
+		if(value="show"){
+			$("#Button1").click(function(){
+				$("p.hide").show(1000);
+			})
+			}
     });
 });
