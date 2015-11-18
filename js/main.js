8a23da5376e17132c5d8307f5258df97855c7aa4
Rana Mansoor Akbar Khan <mansoor@sutlej.net>
2015-11-18 21:49:58 +0500

    Chaining Method and Hover Effect combined

--- js/main.js
+++ js/main.js
@@ -181,8 +181,13 @@ $(document).ready(function(){
         }
     });     
      
-
-     
+// jQuery Hover Effect + Chaining//
+     	
+   $("h1.what-we-do").hover(function(){
+        $(this).css("background-color", "#f16529").css("text-align", "center").css("color", "white");
+        }, function(){
+        $(this).css("background-color", "white").css("text-align", "left").css("color", "black");
+    });
      
      
      
@@ -205,30 +210,18 @@ $(document).ready(function(){
    
 });
  
-// jQuery Hover Effect//	
+	
 $(document).ready(function(){
 	
 	
-	
-   $("h1.what-we-do").hover(function(){
-        $(this).css("background-color", "#f16529");
-		$(this).css("text-align", "center");
-		$(this).css("color", "white");
-        }, function(){
-        $(this).css("background-color", "white");
-		$(this).css("text-align", "left");
-		$(this).css("color", "black");
-    });
+
 });
 // jQuery Click Effect//
 
 $(document).ready(function(){
     $("button").click(function(){
          var div = $("div.animate"); 
-		div.css("background-color","#f16529");
-		div.css("color","white");
-        div.animate({left: '250px',height:'100px',width:'500px'}, "slow");
-        div.animate({fontSize: '3em'}, "slow");
+		div.css("background-color","#f16529").css("color","white").animate({left: '250px',height:'100px',width:'500px'}, "slow").animate({fontSize: '3em'}, "slow");
     });
 });
 
